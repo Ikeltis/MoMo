969ffa90aa819a9b046d193c33356af871141a46
James Swineson <github@public.swineson.me>
2019-05-02 00:46:11 +0800

    fix usability bugs

--- MoMo.py
+++ MoMo.py
@@ -1,97 +1,66 @@
 #encoding: utf8
-
-from requests import *
-import re
+import aiohttp
+import asyncio
 from termcolor import colored
+from requests import get
+import re
 import time
-import threading
+import logging
+import typing
+import sys
 
+logger = logging.getLogger(__name__)
+ip_regex = re.compile(r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}:[0-9]{1,5}")
 
-def getProxy(pnum):
-    global proxies, num, Num
-    while num < Num:
-        if len(proxies) == 0:
-            if B.acquire():
-                print '[+] %s' % colored('get proxy...', 'blue', attrs=['bold']),
-                while 1:
-                    try:
-                        url = 'http://www.89ip.cn/tqdl.html?num=%s' % pnum
-                        html = get(url).text
-                        proxies = set(re.findall(
-                            r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}:[0-9]+", html)) - set(ProxyList)
-                        if not len(proxies):
-                            print '  [-]Waiting'
-                            time.sleep(3)
-                            continue
+def get_proxy_list(target_count: int=100) -> typing.List[str]:
+    proxies = set()
+    while len(proxies) < target_count:
+        try:
+            url = 'http://www.89ip.cn/tqdl.html?num={}'.format(target_count)
+            html = get(url).text
 
-                        print colored('[%d]' % len(proxies), 'yellow', attrs=['bold']), '%s' % (colored('Done!', 'green', attrs=['bold']))
-                        break
-                    except Exception as e:
-                        print('\n  [-]Error: ' + str(e))
-                        time.sleep(5)
-                B.release()
+            new_proxies = set(ip_regex.findall(html))
+            logger.info("Got %d new proxies", len(new_proxies))
+            proxies = proxies.union(new_proxies)
+        except Exception as e:
+            logger.exception(e)
 
+        time.sleep(3)
+    return proxies
 
-def autoVisit():
-    global num, ProxyList, proxies, Num
+async def visit_once(url: str, proxy: str, semaphore: asyncio.Semaphore, timeout: int=5) -> bool:
+    async with semaphore:
+        async with aiohttp.ClientSession() as session:
+            try:
+                async with session.get(url=url, proxy='http://{}'.format(proxy), timeout=timeout) as resp:
+                    logger.info("Proxy %s succeed", proxy)
+                    return True
+            except:
+                logger.warning("Proxy %s failed", proxy)
+                return False
 
-    url = 'http://www.baidu.com'  # your article.
-    headers = {
-        'user-agent': 'Mozilla/5.0 (Linux; Android 7.0; PLUS Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.98 Mobile Safari/537.36'}
+def exception_handler(loop, context):
+    pass
 
-    while num < Num:
-        if A.acquire():
-            # print num
-            if proxies:
-                proxie = proxies.pop()
-                A.release()
-            else:
-                A.release()
-                continue
+def visit_count(url: str, count: int=40, max_concurrency: int=40) -> None:
+    successful_count = 0
+    used_proxies = set()
+    proxies = get_proxy_list()
+    
+    loop = asyncio.get_event_loop()
+    loop.set_exception_handler(exception_handler)
+    sem = asyncio.Semaphore(max_concurrency)
 
-            if proxie not in ProxyList:
-                try:
-                    html = get(url, headers=headers, proxies={
-                               'http': proxie}, timeout=5).text
-                    failed = 0
-                except:
-                    failed = 1
+    while successful_count < count:
+        proxies = get_proxy_list() - used_proxies
+        logger.info("starting batch with %d proxies", len(proxies))
+        tasks = [asyncio.ensure_future(visit_once(url, proxy, sem)) for proxy in proxies]
+        successful_count += len(list(filter(None, loop.run_until_complete(asyncio.gather(*tasks)))))
+        used_proxies = used_proxies.union(proxies)
+        logger.info("%d successful requests using %d proxies", successful_count, len(used_proxies))
 
-                B.acquire()
-                print colored('[%d]' % (num), 'yellow', attrs=['bold']),
-                if failed:
-                    print '[%s] Failed!' % proxie
-                else:
-                    num += 1
-                    print '[%s]' % colored(proxie, 'cyan', attrs=['bold']), colored('Successfully!', 'green', attrs=['bold'])
-                    ProxyList.append(proxie)
-                B.release()
+    loop.close()
 
-
-threads = []
-proxies = []
-A = threading.Lock()
-B = threading.Lock()
-print '-' * 50
-
-ProxyList = []
-num = 0  # already visited
-pnum = 100  # proxies-num
-Num = 40  # visit-num
-
-p = threading.Thread(target=getProxy, args=(pnum,))
-
-for i in range(pnum):
-    t = threading.Thread(target=autoVisit, args=())
-    threads.append(t)
-
-p.start()
-for t in threads:
-    t.start()
-
-p.join()
-for t in threads:
-    t.join()
-
-print '\r', ' ' * 50
-print '%s' % colored('[%s]' % num, 'yellow', attrs=['bold']) + colored(' All done!', 'green', attrs=['bold'])
+if __name__ == "__main__":
+    url = sys.argv[1]
+    visit_count(url)
