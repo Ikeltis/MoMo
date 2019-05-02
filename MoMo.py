01aa83ddb99f11aabef3ac553933f4c839eb36ab
James Swineson <github@public.swineson.me>
2019-05-02 13:23:56 +0800

    better usability

--- MoMo.py
+++ MoMo.py
@@ -1,7 +1,7 @@
-#encoding: utf8
+#!/usr/bin/env python3
+# encoding: utf8
 import aiohttp
 import asyncio
-from termcolor import colored
 from requests import get
 import re
 import time
@@ -10,22 +10,20 @@ import typing
 import sys
 
 logger = logging.getLogger(__name__)
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 ip_regex = re.compile(r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}:[0-9]{1,5}")
 
 def get_proxy_list(target_count: int=100) -> typing.List[str]:
     proxies = set()
-    while len(proxies) < target_count:
-        try:
-            url = 'http://www.89ip.cn/tqdl.html?num={}'.format(target_count)
-            html = get(url).text
+    try:
+        logger.info("Getting proxies...")
+        url = 'http://www.89ip.cn/tqdl.html?num={}'.format(target_count)
+        html = get(url).text
 
-            new_proxies = set(ip_regex.findall(html))
-            logger.info("Got %d new proxies", len(new_proxies))
-            proxies = proxies.union(new_proxies)
-        except Exception as e:
-            logger.exception(e)
-
-        time.sleep(3)
+        proxies = set(ip_regex.findall(html))
+        logger.info("Got %d proxies", len(proxies))
+    except Exception as e:
+        logger.exception(e)
     return proxies
 
 async def visit_once(url: str, proxy: str, semaphore: asyncio.Semaphore, timeout: int=5) -> bool:
@@ -39,28 +37,31 @@ async def visit_once(url: str, proxy: str, semaphore: asyncio.Semaphore, timeout
                 logger.warning("Proxy %s failed", proxy)
                 return False
 
-def exception_handler(loop, context):
-    pass
-
 def visit_count(url: str, count: int=40, max_concurrency: int=40) -> None:
     successful_count = 0
     used_proxies = set()
     proxies = get_proxy_list()
     
     loop = asyncio.get_event_loop()
-    loop.set_exception_handler(exception_handler)
     sem = asyncio.Semaphore(max_concurrency)
 
     while successful_count < count:
-        proxies = get_proxy_list() - used_proxies
-        logger.info("starting batch with %d proxies", len(proxies))
-        tasks = [asyncio.ensure_future(visit_once(url, proxy, sem)) for proxy in proxies]
-        successful_count += len(list(filter(None, loop.run_until_complete(asyncio.gather(*tasks)))))
-        used_proxies = used_proxies.union(proxies)
-        logger.info("%d successful requests using %d proxies", successful_count, len(used_proxies))
+        proxies = get_proxy_list(100 + len(used_proxies)) - used_proxies
+        if len(proxies):
+            logger.info("starting batch with %d proxies", len(proxies))
+            tasks = [asyncio.ensure_future(visit_once(url, proxy, sem)) for proxy in proxies]
+            successful_count += len(list(filter(None, loop.run_until_complete(asyncio.gather(*tasks)))))
+            used_proxies = used_proxies.union(proxies)
+            logger.info("%d successful requests using %d proxies", successful_count, len(used_proxies))
+        else:
+            logger.warning("No new proxies, retrying...")
+            time.sleep(3)
 
     loop.close()
 
 if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: MoMo.py URL", file=sys.stderr)
+        sys.exit(1)
     url = sys.argv[1]
     visit_count(url)
